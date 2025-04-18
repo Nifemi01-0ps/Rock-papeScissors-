a427a0f21f4b76603559c548bb64403a96ee3494
Nifemi01 <raphaeledafesnr@gmail.com>
2025-04-18 11:59:19 +0100

    reference all elements used

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 computerScore = 0;
 humanScore = 0;
 gameOver = false;
+
+// Dom Element Reference
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+const result = document.querySelector("#result");
+const score = document.querySelector("#score");
+const winner = document.querySelector("#winner");
